41138b0904c0f2ceb2879e8e223e7004628fdbf3
ssm00 <97657265+ssm00@users.noreply.github.com>
2023-02-24 15:27:06 +0900

    create

---
+++ springAwsLee/src/main/java/awslee/v1/springawslee/SpringAwsLeeApplication.java
@@ -0,0 +1,13 @@
+package awslee.v1.springawslee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsLeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAwsLeeApplication.class, args);
+    }
+
+}
---
+++ springAwsLee/src/test/java/awslee/v1/springawslee/SpringAwsLeeApplicationTests.java
@@ -0,0 +1,13 @@
+package awslee.v1.springawslee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsLeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
