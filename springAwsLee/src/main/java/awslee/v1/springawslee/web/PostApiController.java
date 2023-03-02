7efca08e238db7a6a83b0d474c62e57969633c41
ssm00 <97657265+ssm00@users.noreply.github.com>
2023-03-02 16:12:00 +0900

    save

--- springAwsLee/src/main/java/awslee/v1/springawslee/web/PostApiController.java
+++ springAwsLee/src/main/java/awslee/v1/springawslee/web/PostApiController.java
@@ -3,9 +3,7 @@ package awslee.v1.springawslee.web;
 import awslee.v1.springawslee.service.posts.PostsService;
 import awslee.v1.springawslee.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +14,11 @@ public class PostApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postService.save(requestDto);
+    }
+
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsSaveRequestDto requestDto) {
+        return postService.update(id, requestDto);
     }
 }
