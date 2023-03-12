15224074ff2bb803bd50b685fe58cc362c6858be
ssm00 <97657265+ssm00@users.noreply.github.com>
2023-03-12 23:19:36 +0900

    table+

--- springAwsLee/src/main/java/awslee/v1/springawslee/config/auth/dto/SessionUser.java
+++ springAwsLee/src/main/java/awslee/v1/springawslee/config/auth/dto/SessionUser.java
@@ -3,10 +3,13 @@ package awslee.v1.springawslee.config.auth.dto;
 import awslee.v1.springawslee.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 
 //인증된 사용자 정보만 필요한 sessionuser
+//세션 직렬화 하여서 사용
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 
     private String name;
     private String email;
